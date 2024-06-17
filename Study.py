1e49410f43c397b7d75f777833f5cba62aa174c3
Andrew <102756686+PenumRas@users.noreply.github.com>
2024-06-17 10:21:09 +0300

    finish?

--- Study.py
+++ Study.py
@@ -1,2 +1,26 @@
 def get_cats_info(path):
-    pass
+    try:
+        with open(path, "r") as fh:
+            lines = [el.strip() for el in fh.readlines()]
+            cat_info = []
+            for line in lines:
+                id, name, age = line.split(",")
+                cats = {
+                    "id": id.strip(),
+                    "name": name.strip(),
+                    "age": age.strip()
+                }
+                cat_info.append(cats)
+                # cat_name.append(name.strip())
+                # cat_age.append(age.strip())
+        return cat_info
+    except FileNotFoundError:
+        print(f"file at path{path} not found")
+        return []
+    except IOError:
+        print(f"some error in read path{path}")
+        return []
+
+cats_info = get_cats_info("cats_file.txt")
+print(cats_info)
+
