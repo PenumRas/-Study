9c5c7c033b71bd970941ef1cf38d72647180cba0
Andrew <102756686+PenumRas@users.noreply.github.com>
2024-06-17 10:36:50 +0300

    new_homework_start

--- Study.py
+++ Study.py
@@ -1,24 +1,4 @@
-def get_cats_info(path):
-    try:
-        with open(path, "r") as fh:
-            lines = [el.strip() for el in fh.readlines()]
-            cat_info = []
-            for line in lines:
-                id, name, age = line.split(",")
-                cats = {
-                    "id": id.strip(),
-                    "name": name.strip(),
-                    "age": age.strip()
-                }
-                cat_info.append(cats)
-        return cat_info
-    except FileNotFoundError:
-        print(f"file at path{path} not found")
-        return []
-    except IOError:
-        print(f"some error in read path{path}")
-        return []
-
-
-cats_info = get_cats_info("cats_file.txt")
-print(cats_info)
+def main():
+    def parse_input():
+        pass
+    pass
