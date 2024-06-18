6fae3a9aedb4745dbdf59a26674e9dedd9cb5429
Andrew <102756686+PenumRas@users.noreply.github.com>
2024-06-18 21:17:17 +0300

    finish

--- Study.py
+++ Study.py
@@ -1,52 +1,43 @@
-telephone_book = {}
+def parse_input(user_input):
+    command, *args = user_input.split()
+    command = command.strip().lower()
+    return command, *args
 
 
-
-def parse_input(command):
-    if " " in command:
-        key_word, *args = command.split(" ", 1)
-    else:
-        return command, None
-    return key_word, *args
-
-
-def command_exit(command):
-    command_exit_words = ["exit", "quit", "leave"]
-    if any(command.lower() == el for el in command_exit_words):
-        return True
+def command_add(args, telephone_book):
+    try:
+        name, telephone = args
+        telephone_book[name] = telephone
+        return telephone_book
+    except (ValueError, AttributeError):
+        print("Try to write command {'add'} and then {'name'} and {'Phone number'} ")
+        return None
 
 
-def command_greeting(command):
-    command_greeting_words = ["hi", "hello", "greet"]
-    if any(command.lower() == el for el in command_greeting_words):
-        return True
-
-
-def command_add(command):
-    key_word, value_mod = parse_input(command)
-    if key_word.lower() == 'add' in key_word:
-        try:
-            name, telephone = value_mod.split(" ", 1)
+def command_change_phone(args, telephone_book):
+    try:
+        name, telephone = args
+        if name in telephone_book:
             telephone_book[name] = telephone
             return telephone_book
-        except (ValueError, AttributeError):
-            print("Try to write command {'add'} and then {'name'} and {'Phone number'} ")
-            return None
-    return False
+        else:
+            print(f"Sorry, but {name} not found in book")
+    except AttributeError:
+        print("Try to write command {'change'} and then {'name'} and {'New Phone number'} ")
 
 
-def command_change_phone(command):
-    key_word, value_mod = parse_input(command)
-    if key_word.lower() == 'change':
-        try:
-            name, telephone = value_mod.split(" ", 1)
-            if name in telephone_book:
-                telephone_book[name] = telephone
-                return telephone_book
-            else:
-                print(f"Sorry, but {name} not found in book")
-        except AttributeError:
-            print("Try to write command {'change'} and then {'name'} and {'New Phone number'} ")
+def command_del(args, telephone_book):
+    name = args[0]
+    if name in telephone_book:
+        del telephone_book[name]
+    return telephone_book
+
+
+def command_phone_by_username(args, telephone_book):
+    name = args[0]
+    if name in telephone_book:
+        return name, telephone_book[name]
+
 
 def command_help():
     return """
@@ -60,33 +51,40 @@ Available commands:
 
 
 def main():
+    telephone_book = {}
     print("Hello, I am personal assistant ")
     while True:
-        command = input("command and values: ")
-        if command_exit(command):
+        user_input = input("command and values: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["exit", "quit", "leave"]:
             break
 
-        elif command_greeting(command):
+        elif command in ["hi", "hello", "greet"]:
             print("Hi, what You want to do?")
 
         elif command == "help":
             print(command_help())
 
-        elif command_change_phone(command):
-            result = command_change_phone(command)
-            if result:
-                print(f"Updated Phone_book is {telephone_book}")
-            if result is None:
-                continue
+        elif command == "add":
+            print(command_add(args, telephone_book))
 
-        elif command_add(command):
-            result = command_add(command)
-            if result:
-                print(f"Updated Phone_book is {telephone_book}")
-            if result is None:
-                continue
+        elif command == "change":
+            print(command_change_phone(args, telephone_book))
+
+        elif command == "del":
+            print(command_del(args, telephone_book))
+
+        elif command == "phone":
+            print(command_phone_by_username(args, telephone_book))
+
+
+        elif command == "all":
+            print(f"List of Nanes and Numbers{telephone_book}")
+
         else:
             print("If Y dont know what I can - print 'Help'")
 
 
-main()
+if __name__ == "__main__":
+    main()
